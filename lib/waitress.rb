8e79b7669123781bfd6a0b930f424aefdc98b566
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-06-21 22:31:07 -0400

    Added ENV variables

--- lib/waitress.rb
+++ lib/waitress.rb
@@ -1,4 +1,9 @@
 require "waitress/version"
+require "pusher"
+
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
 
 module Waitress
   def self.ping_pub_sub_with(message)
