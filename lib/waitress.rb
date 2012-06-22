369801f59fefc04d95d3c3b7ed5138621c6b1185
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-06-22 09:12:04 -0400

    Waitress is a module

--- lib/waitress.rb
+++ lib/waitress.rb
@@ -5,7 +5,7 @@ Pusher.app_id = ENV['PUSHER_APP_ID']
 Pusher.key = ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET']
 
-class Waitress
+module Waitress
   def self.ping_pub_sub_with(message)
     Pusher['ls_trivia'].trigger(message, {})
   end
